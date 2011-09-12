8012f745251123c95e822ea886a06d2e2d9dc81d
Tycho Andersen <tycho@tycho.ws>
2011-09-11 20:05:08 -0500

    Change formatting.

--- gtfo/comments.py
+++ gtfo/comments.py
@@ -9,8 +9,7 @@ _validators = [
 reply_form = form.Form(form.Textbox('name', description='name (required)'),
                        form.Textbox('capcha', description='type orange in this box:'),
                        form.Textbox('url', description='homepage (optional)'),
-                       form.Textbox('email', description="""email
-                          (optional)"""),
+                       form.Textbox('email', description='email (optional)'),
                        form.Textarea('payload', cols=40, rows=10),
                        validators = _validators
                       )
